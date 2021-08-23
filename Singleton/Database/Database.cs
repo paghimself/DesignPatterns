68945fbc80eeedb4623f8a80bd4c48ccb00de4f0
PagHimself <62171485+paghimself@users.noreply.github.com>
2021-08-22 21:10:43 -0400

    Add DP Singleton

---
+++ Singleton/Database/Database.cs
@@ -0,0 +1,23 @@
+﻿namespace Singleton.Database
+{
+    public class Database
+    {
+        private static Database instance;
+        public int ID { get; set; }
+
+        private Database()
+        {
+            ID = 9999;
+        }
+
+        public static Database GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Database();
+            }
+            return instance;
+        }
+
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Singleton.Database;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var db = Database.Database.GetInstance();
+            Console.WriteLine(db.ID);
+            db.ID = 666;
+            Console.WriteLine(db.ID);
+
+            var db2 = Database.Database.GetInstance();
+            db.ID = 13;
+            Console.WriteLine(db.ID);
+        }
+    }
+}
